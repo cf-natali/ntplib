93dc7559e087552021b8384f9fb386a60c9caaa0
programadorhedonista <programadorhedonista@gmail.com>
2020-07-30 22:42:25 +0200

    Revert "add dest_host to NTPStats"
    This reverts commit 2b0059e64f986c8719caf34f244953aecafd15ab.

--- ntplib.py
+++ ntplib.py
@@ -231,7 +231,6 @@ class NTPStats(NTPPacket):
         NTPPacket.__init__(self)
         self.dest_timestamp = 0
         """destination timestamp"""
-        self.dest_host = None
 
     @property
     def offset(self):
@@ -293,7 +292,6 @@ class NTPClient:
         # lookup server address
         addrinfo = socket.getaddrinfo(host, port)[0]
         family, sockaddr = addrinfo[0], addrinfo[4]
-        dest_host = sockaddr[0]
 
         # create the socket
         s = socket.socket(family, socket.SOCK_DGRAM)
@@ -310,7 +308,7 @@ class NTPClient:
 
             # wait for the response - check the source address
             src_addr = None,
-            while src_addr[0] != dest_host:
+            while src_addr[0] != sockaddr[0]:
                 response_packet, src_addr = s.recvfrom(256)
 
             # build the destination timestamp
@@ -324,7 +322,7 @@ class NTPClient:
         stats = NTPStats()
         stats.from_data(response_packet)
         stats.dest_timestamp = dest_timestamp
-        stats.dest_host = dest_host
+
         return stats
 
 
